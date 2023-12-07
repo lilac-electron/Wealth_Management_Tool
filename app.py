5944832f0ecce832cbce4ce5b082da5e433d93af
lilac-electron <finpearson@gmail.com>
2023-12-07 21:15:58 +0000

    first html stuff

---
+++ app.py
@@ -0,0 +1,37 @@
+
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import io
+import base64
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+
+class MyForm(FlaskForm):
+    name = StringField('Name')
+    email = StringField('Email')
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = MyForm()
+
+    if form.validate_on_submit():
+        # Access form data here (e.g., form.name.data, form.email.data)
+        return "Form submitted with Name: {form.name.data}, Email: {form.email.data}"
+
+    return render_template('auth/index.html', form=form)
+
+@app.route('/graph')
+def graph():
+    df = pd.read_excel('example.xlsx')
+    # Render the HTML template with the base64-encoded image
+    table_html = df.to_html(classes='table table-striped', index=False)
+    return render_template('viewData/graph.html', table_html=table_html)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- templates/auth/index.php
+++ templates/auth/index.php
@@ -1,23 +1,16 @@
-<?php require_once("../includes/head.php")?>
-<?php require_once("../includes/nav.php")?>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<link rel="stylesheet" href="{{ url_for('static', filename='css/bootstrap.css')}}">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../static/bootstrap.css">
+    
+    <title>Wealth Management Tool !</title>
 </head>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">Wealth Management Tool Python</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                </li>
-                </ul>
-                <a href="admin.php"><button class="btn btn-dark mr-1">Admin Page</button></a>
-                <a href="newEval.php"><button class="btn btn-dark mr-1">New Eval</button></a>
-        </div>
-    </nav>
+<body style="background:#CCC">
+
+
     
 <body>
 <div class="container">
