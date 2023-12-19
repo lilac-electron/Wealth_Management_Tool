91b761c757b93cc6b86b791c0175a194ce1e9f6e
lilac-electron <finpearson@gmail.com>
2023-12-19 20:43:33 +0000

    Generate Excel doc
    In this push there are a few additions such as opening a bank document. Then attempting to generate a banking document to look similar to a real banking document

---
+++ generateExcelFile.py
@@ -0,0 +1,61 @@
+import openpyxl
+from datetime import datetime, timedelta
+import random
+
+def is_payday(transaction_date, pay_day):
+    #print(pay_day.day)
+    #print(transaction_date.day)
+    return transaction_date.day == pay_day.day
+
+def generate_excel_file(file_path, num_transactions=600, outbound_frequency=0.7, pay_day=1, pay_day_amount=1400):
+    # Create a new workbook and select the active sheet
+    workbook = openpyxl.Workbook()
+    sheet = workbook.active
+    
+    # Add headers
+    headers = ["Date", "Description", "Money In", "Money Out", "Balance"]
+    sheet.append(headers)
+    
+    # Generate diverse sample data
+    transaction_date = datetime(2023, 1, 1)
+    current_balance = 0
+    
+    for _ in range(num_transactions):
+        # Generate a random date within a range
+        transaction_date = transaction_date + timedelta(days=random.randint(0, 1))
+
+        if is_payday(transaction_date, datetime(2023, transaction_date.month, pay_day)):
+            current_balance += pay_day_amount
+            sheet.append([transaction_date.strftime('%Y-%m-%d'), "Payday", pay_day_amount, 0, current_balance])
+            
+        
+        # Determine if it's an inbound or outbound transaction
+        if random.random() < outbound_frequency:
+            transaction_date = transaction_date + timedelta(days=random.randint(0, 2))
+            # Outbound transaction
+            description = f"Expense {random.randint(1, 10)}"
+            #money_out = random.randint(1, 500)
+            money_out = random.randint(1, 70)
+            money_in = 0
+            current_balance -= money_out
+        """else:
+            # Inbound transaction
+            description = f"Income {random.randint(1, 10)}"
+            #money_in = random.randint(500, 1000)
+            money_in = 1000
+            money_out = 0
+            current_balance += money_in"""
+        
+        # Append the transaction to the sheet
+        sheet.append([transaction_date.strftime('%Y-%m-%d'), description, money_in, money_out, current_balance])
+    
+    # Save the workbook to the specified file path
+    workbook.save(file_path)
+
+# Specify the path where you want to save the Excel file
+excel_file_path = 'generated_file_with_600_transactions_v4.xlsx'
+
+# Call the function to generate the Excel file with 600 transactions
+generate_excel_file(excel_file_path, num_transactions=600, outbound_frequency=0.97)
+
+print(f"Excel file generated at: {excel_file_path}")
--- test.py
+++ test.py
@@ -1,3 +1,46 @@
-import pandas as pd
-df = pd.read_excel('example.xlsx', sheet_name='Sheet1')
-print(df)
+#import pandas as pd
+#df = pd.read_excel('example.xlsx', sheet_name='Sheet1')
+#print(df)
+
+import openpyxl
+
+def read_excel_file(file_path):
+    # Load the workbook
+    workbook = openpyxl.load_workbook(file_path)
+    
+    # Assuming the data is in the first sheet (you can modify this accordingly)
+    sheet = workbook.active
+    
+    # Dictionary to store the extracted data
+    data_list = []
+    
+    # Iterate through rows starting from the second row (assuming the first row is headers)
+    for row in sheet.iter_rows(min_row=6, values_only=True):
+        date, description, money_in, money_out, balance = row
+        
+        # Add money_in, money_out, and balance to the dictionary
+        data_list.append((date, money_in, money_out, balance, description))
+        #print(len(data_list))
+    
+    return data_list
+
+# Example usage
+file_path = 'Statement_2023.xlsx'
+result_list = read_excel_file(file_path)
+
+# Print the result
+total_out = 0
+total_in = 0
+total_balance = 0
+for data_tuple in result_list:
+    date, money_in, money_out, balance, description = data_tuple
+    #print(f"Date: {date}, Money In: {money_in}, Money Out: {money_out}, Balance: {balance}, Description: {description}")
+    #print (str(money_in))
+    total_in += float(money_in) if money_in is not None else 0
+    total_out += float(money_out) if money_out is not None else 0
+    total_balance += float(balance) if balance is not None else 0
+
+print(total_in)
+print(total_out)
+print(total_in-total_out)
+print(balance)
