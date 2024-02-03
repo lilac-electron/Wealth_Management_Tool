e945d245cd89c36d77ecff522fb888eb0a5ca311
lilac-electron <finpearson@gmail.com>
2024-02-03 17:22:55 +0000

    Many Many changes
    Many changes were made to the code, not a lot of them good ones. This is not a happy commit. Currently the code works. But I am displeased.

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask, render_template
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms import StringField, PasswordField, SubmitField, validators, BooleanField, SelectMultipleField
 from flask_wtf.file import FileField, FileAllowed, MultipleFileField
-from wtforms.validators import InputRequired, Email, Length
+from wtforms.validators import InputRequired, Email, Length, NumberRange
 import email_validator
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -59,9 +59,75 @@ class DeleteFileForm(FlaskForm):
     files_to_delete = SelectMultipleField('Files to Delete', coerce=str)
     submit = SubmitField('Delete Selected Files')
 
+class DynamicForm(FlaskForm):
+    pass
+class DynamicForm2(FlaskForm):
+    pass
+def clearAttribute():
+    DynamicForm = [attr for attr in DynamicForm if  not(attr.startswith('field_'))]
+
+def CreditsForm(inputs_list):
+    for item in inputs_list:
+        setattr(DynamicForm, f'field_{item}', IntegerField(f'Enter monthly payment for {item}', validators=[validators.NumberRange(min=0)]))
+        #print(item)
+    #DynamicForm.submit = SubmitField('Submit')   
+
+def AssetForm(inputs_list):
+    for item in inputs_list:
+        setattr(DynamicForm2, f'field_{item}', IntegerField(f'Enter asset value for {item}', validators=[validators.NumberRange(min=0)]))
+        #print(item)   
+
 def list_files(directory):
     file_names = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
     return file_names
+
+def create_csv_file(file_path, column_names, data_dict):
+    with open(file_path, 'w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=column_names)
+        writer.writeheader()
+        writer.writerow(data_dict)
+
+def read_csv_file(file_path):
+    data_dict = {}
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            data_dict.update(row)
+    return data_dict
+
+def write_csv_file(upload_folder_path, data):
+    # Ensure the folder exists (create it if it doesn't)
+    #os.makedirs(upload_folder_path, exist_ok=True)
+
+    column_names = list(data.keys())
+    print("THESE ARE COLUMN NAMES")
+    print(column_names)
+    print(type(data))
+
+    # Open the CSV file for writing
+    with open(upload_folder_path, 'w', newline='') as file:
+        writer = csv.writer(file)
+
+        # Write the header row (keys)
+        writer.writerow(data.keys())
+
+        # Write the data row (values)
+        writer.writerow(data.values())
+
+def read_user_data(username, upload_folder_path):
+    # Create asset and credit file paths
+    csv_asset_upload_folder_path = os.path.join(upload_folder_path, f'{username}_assetValue.csv')
+    csv_credit_upload_folder_path = os.path.join(upload_folder_path, f'{username}_credits.csv')
+
+    # Read values from asset and credit CSV files
+    asset_data = read_csv_file(csv_asset_upload_folder_path)
+    credit_data = read_csv_file(csv_credit_upload_folder_path)
+
+    # Create dictionaries for assets and credits
+    #assets_dict = {'assets': asset_data}
+    #credits_dict = {'credits': credit_data}
+
+    return asset_data, credit_data
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -72,18 +138,63 @@ def index():
 def dashboard():
     return render_template('pages/dashboard.html', name=current_user.username)
 
-@app.route('/credits')
+@app.route('/credits', methods=['GET', 'POST'])
 @login_required
 def credits():
-    return render_template('pages/credits.html', name=current_user.username)
+    #input_list = ['value1', 'value2', 'value3']  # Replace with your list
+    input_list = app.config['CREDITS'].keys()
+    form = DynamicForm()
+    upload_folder_path = os.path.join('upload_folder', f'{current_user.username}/{current_user.username}_credits.csv')
+    print(upload_folder_path)
+    if request.method == 'POST' and form.validate_on_submit():
+        entered_data = {key.lstrip('field_'): value for key, value in request.form.items() if key.startswith('field_')}
+        print("Entered data:", entered_data)
+        app.config['CREDITS'] = entered_data
+        write_csv_file(upload_folder_path, entered_data)
+        ##print("Entered data:", entered_data)
+        flash("Data entered successfully", "success")
+        #clearAttribute()
+
+    if request.method == 'POST' and not form.validate_on_submit():
+        flash('There was an issue uploading your data, please try again', 'danger')
+
+    CreditsForm(input_list)
+    total_credits = 0
+    for key in input_list:
+        total_credits += int(app.config['CREDITS'][key])
+    return render_template('pages/credits.html', name=current_user.username, form=form, credits_data = app.config['CREDITS'], total_credits=total_credits)
+
 @app.route('/simulatedGrowth')
 @login_required
 def simulatedGrowth():
     return render_template('pages/simulatedGrowth.html', name=current_user.username)
-@app.route('/assetValue')
+@app.route('/assetValue', methods=['GET', 'POST'])
 @login_required
 def assetValue():
-    return render_template('pages/currentAssetValue.html', name=current_user.username)
+    #input_list = ['value1', 'value2', 'value3']  # Replace with your list
+    input_list = app.config['ASSETS'].keys()
+    form =  DynamicForm2()
+    upload_folder_path = os.path.join('upload_folder', f'{current_user.username}/{current_user.username}_assetValue.csv')
+    print(upload_folder_path)
+    if request.method == 'POST' and form.validate_on_submit():
+        print("test 1")
+        entered_data = {key.lstrip('field_'): value for key, value in request.form.items() if key.startswith('field_')}
+        print("test 1")
+        app.config['ASSETS'] = entered_data
+        print("test 1")
+        write_csv_file(upload_folder_path, entered_data)
+        print("test 1")
+        #print("Entered data:", entered_data)
+        flash("Data entered successfully", "success")
+
+    if request.method == 'POST' and not form.validate_on_submit():
+        flash('There was an issue uploading your data, please try again', 'danger')
+
+    AssetForm(input_list)
+    total_assets = 0
+    for key in input_list:
+        total_assets += int(app.config['ASSETS'][key])
+    return render_template('pages/currentAssetValue.html', name=current_user.username, form=form, assets_data=app.config['ASSETS'], total_assets=total_assets)
 @app.route('/updateFinances')
 @login_required
 def updateFinances():
@@ -103,15 +214,19 @@ def login():
 
                 # Set the UPLOAD_FOLDER configuration
                 app.config['UPLOAD_FOLDER'] = upload_folder_path
+                assets, credits = read_user_data(username, upload_folder_path)
+                app.config['ASSETS'] = assets
+                app.config['CREDITS'] = credits
+                #print(assets)
+                #print(credits)
                 flash("Welcome "+ str(current_user.username)+", you have been logged in.")
                 return redirect(url_for('dashboard'))
 
-        return '<h1>Invalid Username or Password</h1>'
+        flash("Incorrect Username or Password", "danger")
+        #return '<h1>Invalid Username or Password</h1>'
         #return '<h1>' + form.username.data + ' ' + form.password.data + '</h1>'
 
     return  render_template('login/login.html',form=form)
-
-
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -133,16 +248,27 @@ def register():
         # Set the UPLOAD_FOLDER configuration
         app.config['UPLOAD_FOLDER'] = upload_folder_path
         #Create a user csv which will store their asset values
-        with open(csv_asset_upload_folder_path, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['House', 'Car', 'Investments', 'Checking Account', 'Stocks', 'Savings', 'Retirement Accounts'])
-            writer.writerow([])
+        #with open(csv_asset_upload_folder_path, 'w', newline='') as file:
+        #    writer = csv.writer(file)
+        #    writer.writerow(['House', 'Car', 'Investments', 'Checking Account', 'Stocks', 'Savings', 'Retirement Accounts'])
+        #    writer.writerow([])
 
-        with open(csv_credit_upload_folder_path, 'w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(['Rent', 'Mortgage', 'Utilities', 'Food and Groceries','Car Payments', 'Student loan Payments', 'Pension','Streaming Subscriptions', 'Music Subscriptions', 'Misc Subscriptions', 'Health Insurance', 'House Insurance', 'Other Insurance'])
-            writer.writerow([20, 400])
+        #with open(csv_credit_upload_folder_path, 'w', newline='') as file:
+        #    writer = csv.writer(file)
+        #    writer.writerow(['Rent', 'Mortgage', 'Utilities', 'Food and Groceries','Car Payments', 'Student loan Payments', 'Pension','Streaming Subscriptions', 'Music Subscriptions', 'Misc Subscriptions', 'Health Insurance', 'House Insurance', 'Other Insurance'])
+        #    writer.writerow([20, 400])
 
+        # Define column names for asset and credit files
+        asset_column_names = ['House', 'Car', 'Investments', 'Checking Account', 'Stocks', 'Savings', 'Retirement Accounts']
+        credit_column_names = ['Rent', 'Mortgage', 'Utilities', 'Food and Groceries', 'Car Payments', 'Student loan Payments', 'Pension', 'Streaming Subscriptions', 'Music Subscriptions', 'Misc Subscriptions', 'Health Insurance', 'House Insurance', 'Other Insurance']
+
+        # Create dictionaries with column names as keys and 0 as values
+        asset_data_dict = dict.fromkeys(asset_column_names, 0)
+        credit_data_dict = dict.fromkeys(credit_column_names, 0)
+
+        # Create asset and credit files with dictionaries
+        create_csv_file(csv_asset_upload_folder_path, asset_column_names, asset_data_dict)
+        create_csv_file(csv_credit_upload_folder_path, credit_column_names, credit_data_dict)
         flash('New User Created', 'success')
         return redirect (url_for('login'))
         #return '<h1>' + form.email.data + ' ' + form.username.data + ' ' + form.password.data + '</h1>'
@@ -207,10 +333,6 @@ def delete_files():
 def logout():
     logout_user()
     return redirect(url_for('index'))
-
-
-
-
 @app.route('/graph')
 def graph():
     df = pd.read_excel('example.xlsx')
@@ -227,6 +349,10 @@ def graph():
     # Render the HTML template with the base64-encoded image
     #table_html = df.to_html(classes='table table-striped', index=False)
     return render_template('viewData/graph.html', labels=labels, values=values)
+
+@app.route('/monzo_test')
+def monzo():
+    return render_template('pages/monzo_test.html')
 
 with app.app_context():
     #db.drop_all()
