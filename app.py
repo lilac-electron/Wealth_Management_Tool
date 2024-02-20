b56b88efc335597b7b42c356a9284da5dafec8e8
lilac-electron <finpearson@gmail.com>
2024-02-20 13:09:47 +0000

    import

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from wtforms import (
 from wtforms.validators import (
     InputRequired, Email, Length, NumberRange, validators
 )
-from flask_wtf.file import FileField, FileAllowed#, MultipleFileField
+#from flask_wtf.file import FileField, FileAllowed, MultipleFileField
+from wtforms import MultipleFileField
 import email_validator
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
