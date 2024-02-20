098f836e8f32ab784c636a7bf9e7d91e32b7e60f
lilac-electron <finpearson@gmail.com>
2024-02-20 13:19:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,6 @@ from wtforms.validators import (
 )
 
 from wtforms import MultipleFileField
-import email_validator
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
     LoginManager, UserMixin, login_user, login_required, logout_user, current_user
