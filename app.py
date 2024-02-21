2c11a9c4cc0366f5cdcdf5620c33339d57addb08
lilac-electron <finpearson@gmail.com>
2024-02-21 08:21:17 +0000

    removing other credits to try login first

--- app.py
+++ app.py
@@ -322,13 +322,13 @@ def login():
             if check_password_hash(user.password, form.password.data):
                 login_user(user, remember=form.remember.data)
                 username = current_user.username
-                upload_folder_path = os.path.join('upload_folder', username)
+                #upload_folder_path = os.path.join('upload_folder', username)
 
                 # Set the UPLOAD_FOLDER configuration
-                app.config['UPLOAD_FOLDER'] = upload_folder_path
-                assets, credits = read_user_data(username, upload_folder_path)
-                app.config['ASSETS'] = assets
-                app.config['CREDITS'] = credits
+                #app.config['UPLOAD_FOLDER'] = upload_folder_path
+                #assets, credits = read_user_data(username, upload_folder_path)
+                #app.config['ASSETS'] = assets
+                #app.config['CREDITS'] = credits
                 #print(assets)
                 #print(credits)
                 flash("Welcome "+ str(current_user.username)+", you have been logged in.")
