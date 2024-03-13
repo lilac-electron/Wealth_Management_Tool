5a37f8c1e764b66a9cdfa4a8f80f1c9bab780ec7
lilac-electron <finpearson@gmail.com>
2024-03-13 14:10:58 +0000

    Update app.py

--- app.py
+++ app.py
@@ -443,8 +443,9 @@ def credits():
     #upload_folder_path = os.path.join('Wealth_Managment_Tool/upload_folder', f'{current_user.username}/{current_user.username}_credits.csv')
     #print(upload_folder_path)
     if request.method == 'POST' and form.validate_on_submit():
+        print("Entered data:", entered_data)
         entered_data = {key.lstrip('field_'): value for key, value in request.form.items() if key.startswith('field_')}
-        #print("Entered data:", entered_data)
+        print("Entered data:", entered_data)
         app.config['CREDITS'] = entered_data
         #write_csv_file(upload_folder_path, entered_data)
         write_dict_to_excel(app.config['UPLOAD_FOLDER'], 'credits', entered_data)
