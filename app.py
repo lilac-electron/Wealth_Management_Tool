e4a7da3f5824ca5db8ed8ab2530b9e90c30ca537
lilac-electron <finpearson@gmail.com>
2024-03-14 13:20:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -570,9 +570,9 @@ def simulatedGrowth():
         SPdata.append(SP_asset_val)
         LR_asset_val *= (1 + ((np.random.normal(0.65, 0.35,1)[0]/12)/100))
         LRdata.append(LR_asset_val)
-        S_asset_val *= (1 + ((np.random.normal(7, 3,1)[0]/12)/100))
+        S_asset_val *= (1 + ((np.random.normal(7, 4,1)[0]/12)/100))
         Sdata.append(S_asset_val)
-        HR_asset_val *= (1+ ((np.random.normal(10, 9,1)[0]/12)/100))
+        HR_asset_val *= (1+ ((np.random.normal(7, 20,1)[0]/12)/100))
         HRdata.append(HR_asset_val)
     return render_template('pages/simulatedGrowth.html', name=current_user.username, stock_table = stock_data_html, labels=labels, SPdata=SPdata, Sdata=Sdata, LRdata=LRdata, HRdata=HRdata)
 
