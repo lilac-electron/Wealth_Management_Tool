b16de22213348badd6efdffa779497e11e9cefea
lilac-electron <finpearson@gmail.com>
2024-03-29 14:09:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -699,69 +699,75 @@ def register():
     if form.validate_on_submit():
         hashed_password = sha256_crypt.hash(form.password.data)
         print(hashed_password)
-        #new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        db.session.add(new_user)
-        db.session.commit()
         username = form.username.data
-        upload_folder_path = os.path.join('Wealth_Managment_Tool/upload_folder', username)
-        #csv_asset_upload_folder_path = os.path.join(upload_folder_path, f'{username}_assetValue.csv')
-        #csv_credit_upload_folder_path = os.path.join(upload_folder_path, f'{username}_credits.csv')
+        email = form.email.data
+        existing_user = User.query.filter((User.username == username) | (User.email == email)).first()
+        if existing_user:
+            flash('Username or email alreaddy in use, please try again', 'danger')
+        else:
+            #new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+            new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+            db.session.add(new_user)
+            db.session.commit()
+            username = form.username.data
+            upload_folder_path = os.path.join('Wealth_Managment_Tool/upload_folder', username)
+            #csv_asset_upload_folder_path = os.path.join(upload_folder_path, f'{username}_assetValue.csv')
+            #csv_credit_upload_folder_path = os.path.join(upload_folder_path, f'{username}_credits.csv')
 
-        # Ensure the folder exists (create it if it doesn't)
-        os.makedirs(upload_folder_path, exist_ok=True)
-        upload_folder_path = os.path.join(upload_folder_path, f'{username}_values.xlsx')
-        # Set the UPLOAD_FOLDER configuration
-        app.config['UPLOAD_FOLDER'] = upload_folder_path
-        #Create a user csv which will store their asset values
-        #with open(csv_asset_upload_folder_path, 'w', newline='') as file:
-        #    writer = csv.writer(file)
-        #    writer.writerow(['House', 'Car', 'Investments', 'Checking Account', 'Stocks', 'Savings', 'Retirement Accounts'])
-        #    writer.writerow([])
+            # Ensure the folder exists (create it if it doesn't)
+            os.makedirs(upload_folder_path, exist_ok=True)
+            upload_folder_path = os.path.join(upload_folder_path, f'{username}_values.xlsx')
+            # Set the UPLOAD_FOLDER configuration
+            app.config['UPLOAD_FOLDER'] = upload_folder_path
+            #Create a user csv which will store their asset values
+            #with open(csv_asset_upload_folder_path, 'w', newline='') as file:
+            #    writer = csv.writer(file)
+            #    writer.writerow(['House', 'Car', 'Investments', 'Checking Account', 'Stocks', 'Savings', 'Retirement Accounts'])
+            #    writer.writerow([])
 
-        #with open(csv_credit_upload_folder_path, 'w', newline='') as file:
-        #    writer = csv.writer(file)
-        #    writer.writerow(['Rent', 'Mortgage', 'Utilities', 'Food and Groceries','Car Payments', 'Student loan Payments', 'Pension','Streaming Subscriptions', 'Music Subscriptions', 'Misc Subscriptions', 'Health Insurance', 'House Insurance', 'Other Insurance'])
-        #    writer.writerow([20, 400])
+            #with open(csv_credit_upload_folder_path, 'w', newline='') as file:
+            #    writer = csv.writer(file)
+            #    writer.writerow(['Rent', 'Mortgage', 'Utilities', 'Food and Groceries','Car Payments', 'Student loan Payments', 'Pension','Streaming Subscriptions', 'Music Subscriptions', 'Misc Subscriptions', 'Health Insurance', 'House Insurance', 'Other Insurance'])
+            #    writer.writerow([20, 400])
 
-        # Define column names for asset and credit files
-        # Defining them in this way makes the forms more versatile
-        asset_column_names = ['House', 'Car', 'Investments', 'Checking Account', 'Stocks', 'Savings', 'Retirement Accounts']
-        credit_column_names = ['Rent', 'Mortgage', 'Utilities', 'Food and Groceries', 'Car Payments', 'Student loan Payments', 'Pension', 'Subscriptions', 'Health Insurance', 'House Insurance', 'Other Insurance']
-        savings_column_names = ['savings_goal', 'saving_per_month', 'current_savings', 'annual_interest_rate', 'years_to_save']
-        retirement_column_names = ['current_age','desired_retirement_age','current_savings','expected_annual_return','desired_annual_income']
-        tax_column_names = ['annual_salary','additional_income','deductible_expenses','tax_year','filing_status']
-        capital_gains_column_names = ['purchase_price','sale_price','holding_period','cost_of_improvements','deductions_exemptions']
+            # Define column names for asset and credit files
+            # Defining them in this way makes the forms more versatile
+            asset_column_names = ['House', 'Car', 'Investments', 'Checking Account', 'Stocks', 'Savings', 'Retirement Accounts']
+            credit_column_names = ['Rent', 'Mortgage', 'Utilities', 'Food and Groceries', 'Car Payments', 'Student loan Payments', 'Pension', 'Subscriptions', 'Health Insurance', 'House Insurance', 'Other Insurance']
+            savings_column_names = ['savings_goal', 'saving_per_month', 'current_savings', 'annual_interest_rate', 'years_to_save']
+            retirement_column_names = ['current_age','desired_retirement_age','current_savings','expected_annual_return','desired_annual_income']
+            tax_column_names = ['annual_salary','additional_income','deductible_expenses','tax_year','filing_status']
+            capital_gains_column_names = ['purchase_price','sale_price','holding_period','cost_of_improvements','deductions_exemptions']
 
-        sheet_data = {
-            'credits': credit_column_names,  # List for 'credits' sheet
-            'assets': asset_column_names,      # List for 'assets' sheet
-            'retirement': retirement_column_names,  # List for 'retirement' sheet
-            'savings': savings_column_names, # List for 'savings' sheet
-            'tax': tax_column_names, #List for 'tax' sheet
-            'capital gains': capital_gains_column_names
-        }
-        # Create dictionaries with column names as keys and 0 as values
-        #asset_data_dict = dict.fromkeys(asset_column_names, 0)
-        #credit_data_dict = dict.fromkeys(credit_column_names, 0)
+            sheet_data = {
+                'credits': credit_column_names,  # List for 'credits' sheet
+                'assets': asset_column_names,      # List for 'assets' sheet
+                'retirement': retirement_column_names,  # List for 'retirement' sheet
+                'savings': savings_column_names, # List for 'savings' sheet
+                'tax': tax_column_names, #List for 'tax' sheet
+                'capital gains': capital_gains_column_names
+            }
+            # Create dictionaries with column names as keys and 0 as values
+            #asset_data_dict = dict.fromkeys(asset_column_names, 0)
+            #credit_data_dict = dict.fromkeys(credit_column_names, 0)
 
-        # Create asset and credit files with dictionaries
-        #create_csv_file(csv_asset_upload_folder_path, asset_column_names, asset_data_dict)
-        #create_csv_file(csv_credit_upload_folder_path, credit_column_names, credit_data_dict)
-        create_excel_file(app.config['UPLOAD_FOLDER'], sheet_data=sheet_data)
-        finance_path = 'Wealth_Managment_Tool/SimulatedFinanceData/' + username
-        os.makedirs(finance_path, exist_ok=True)
+            # Create asset and credit files with dictionaries
+            #create_csv_file(csv_asset_upload_folder_path, asset_column_names, asset_data_dict)
+            #create_csv_file(csv_credit_upload_folder_path, credit_column_names, credit_data_dict)
+            create_excel_file(app.config['UPLOAD_FOLDER'], sheet_data=sheet_data)
+            finance_path = 'Wealth_Managment_Tool/SimulatedFinanceData/' + username
+            os.makedirs(finance_path, exist_ok=True)
 
-        generator = FinanceDataGenerator()
-        generator.generate_and_save_json(finance_path + '/current_account_transactions.json')
+            generator = FinanceDataGenerator()
+            generator.generate_and_save_json(finance_path + '/current_account_transactions.json')
 
-        generator = CreditCardDataGenerator()
-        generator.generate_and_save_json(finance_path + '/credit_card_transactions.json')
-        app.config['FINANCE_PATH'] = finance_path
+            generator = CreditCardDataGenerator()
+            generator.generate_and_save_json(finance_path + '/credit_card_transactions.json')
+            app.config['FINANCE_PATH'] = finance_path
 
-        flash('New User Created', 'success')
-        return redirect(url_for('login'))
-        #return '<h1>' + form.email.data + ' ' + form.username.data + ' ' + form.password.data + '</h1>'
+            flash('New User Created', 'success')
+            return redirect(url_for('login'))
+            #return '<h1>' + form.email.data + ' ' + form.username.data + ' ' + form.password.data + '</h1>'
 
     return render_template('login/register.html',form=form)
 
