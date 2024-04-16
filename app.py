bdaab682518ff524880a117aa9c9cfe20fcda8d3
lilac-electron <finpearson@gmail.com>
2024-04-16 23:07:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1269,14 +1269,16 @@ def mortgageCalculator():
     if request.method == "POST":
         if form.validate_on_submit():
             loan_amount = form.loan_amount.data
-            print(loan_amount)
+            #print(loan_amount)
             down_payment = form.down_payment.data
-            print(down_payment)
+            if down_payment == None:
+                down_payment = 0
+            #print(down_payment)
             interest_rate = form.interest_rate.data
-            print(interest_rate)
+            #print(interest_rate)
             loan_term = form.loan_term.data
-            print(loan_term)
-            #monthly_payment = round(mortgageRepaymentCalculator(loan_amount, interest_rate, loan_term, down_payment), 2)
+            #print(loan_term)
+            monthly_payment = round(mortgageRepaymentCalculator(loan_amount, interest_rate, loan_term, down_payment), 2)
             #print(monthly_payment)
 
     return render_template('pages/mortgageRepayment.html', form=form, monthly_payment=monthly_payment)
